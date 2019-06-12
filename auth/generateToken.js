7a4d4604fc0986d5ee4b77a3bfd5554b5db139ce
Jonathan Taylor <jonyonson@icloud.com>
2019-06-12 15:07:53 -0500

    finished day 1 requirements

---
+++ auth/generateToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const secret = require('../config/secrets').jwtSecret;
+
+module.exports = user => {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    departments: ['finance'],
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secret, options);
+};
